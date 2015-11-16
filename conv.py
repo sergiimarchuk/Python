2d072d774ceb7393858bb8e00b7a5f04582fced5
sergiimarchuk <sergii.marchuk@gmail.com>
2015-11-16 12:41:57 +0200

    Create conv.py

---
+++ conv.py
@@ -0,0 +1,12 @@
+##### conv text list to list
+
+#!/usr/bin/python
+
+with open("conv-txt-list.py", "rb") as fp:
+        lines = []
+        for line in fp:
+                #print len(line)
+                if len(line) <> 1:
+                        if (line not in lines):
+                                lines.append(line[:-1]) if line[-1] == "\n" else lines.append(line)
+        print lines
