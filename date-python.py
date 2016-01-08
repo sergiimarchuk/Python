24b29303508f14cbe331493c7bdb65c0e4237573
sergiimarchuk <sergii.marchuk@gmail.com>
2016-01-08 16:19:15 +0200

    Update date-python.py

--- date-python.py
+++ date-python.py
@@ -1,5 +1,15 @@
 
 <pre>
->>> import datetime
+>>> import subprocess, sys, datetime, time, csv
+>>> #Get current date and time
+... def get_datetime():
+...         localtime = time.localtime(time.time())
+...         datetime = str(localtime[0]) + """-""" + str(localtime[1]) + """_"""  + str(localtime[2]) + '-'  + str(localtime[3]) + '-' + str(localtime[4])
+...         return(datetime)
+...
+>>>
+>>> get_datetime()
+'2016-1_8-16-18'
+
 
 </pre>
