b3aa1ff865c41b805dde4a618e9d266109c6d0d6
sergiimarchuk <sergii.marchuk@gmail.com>
2016-10-27 14:34:34 +0300

    Create list_element_repeat.py

---
+++ list_element_repeat.py
@@ -0,0 +1,38 @@
+lis = [0,3,4,3,5,4,4,1,5,1,1,0,7,7,8,7,4,5,6]
+print lis,'\n'
+
+l = len(lis)
+c = 0 
+
+dic = {}
+for i in lis:
+        count = 0
+       
+        for j in lis:
+                #print lis[c]
+                if j == i:
+                        count = count + 1
+                        c = c + 1
+      
+        #print i,'got it','count = ',count
+        #print i
+        dic[i]=count
+#print dic
+for key in dic:
+        print 'element :', key, 'count repeat', dic[key]
+        
+ <pre>
+  OUTPUT :
+  </pre>
+<pre>  
+  [0, 3, 4, 3, 5, 4, 4, 1, 5, 1, 1, 0, 7, 7, 8, 7, 4, 5, 6] 
+
+element : 0 count repeat 2
+element : 1 count repeat 3
+element : 3 count repeat 2
+element : 4 count repeat 4
+element : 5 count repeat 3
+element : 6 count repeat 1
+element : 7 count repeat 3
+element : 8 count repeat 1
+</pre>
