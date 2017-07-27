99295b25e483d19da0be378e7e49aaec38a209ce
sergiimarchuk <sergii.marchuk@gmail.com>
2017-07-27 13:48:37 +0300

    Update compare_yml.py

--- compare_yml.py
+++ compare_yml.py
@@ -1,50 +1,44 @@
 #!/usr/bin/python
 import sys
 import yaml
+
 def main():
-        gold_standard = sys.argv[1]
-        input_file = sys.argv[2]
-        output_file = sys.argv[3]
-        print(gold_standard,input_file,output_file)
-
-        with open(gold_standard, 'r') as g_s:
-                g_s_config = yaml.load(g_s)
-
-        with open(input_file, 'r') as i_f:
-                i_f_config = yaml.load(i_f)
-        for key in g_s_config["ora_asm"][0]:
-                #print("aaa")
-                #print(key)
-                #print(type(g_s_config["ora_asm"][0][key]))
-                temp_type = type(g_s_config["ora_asm"][0][key])
-                if (temp_type is list) or (temp_type is str):
-                        #print("bbb")
-                        #print("step A: " + key)
-                        print(type(g_s_config["ora_asm"][0][key]))
-                        ### Level 2 ###
-                        if type(key) is list:
-                                for element in range(len(g_s_config["ora_asm"][0][key])):
-                                        print("step B: " + str(element))
-                                        ### Level 3 ###
-                                        #print(type(g_s_config["ora_asm"][0][key]))
-                                        if type(element) is list:
-                                                print("Step E: " + element)
-                                                for item in g_s_config["ora_asm"][0][key][element]:
-                                                        print("Step D: " + item)
-                                                        if item not in i_f_config["ora_asm"][0][key][element]:
-                                                                #print(type(g_s_config["ora_asm"][0][key][element]))
-                                                                print("aaa")
-                                                                i_f_config["ora_asm"][0][key][element][item] = ''
-                        if type(key) is str:
-                                #print("Step C: " + key)
-                                if key not in i_f_config["ora_asm"][0]:
-                                        i_f_config["ora_asm"][0][key] = ''
-        with open(output_file, 'w') as o_f:
-                o_f.write(yaml.dump(i_f_config, default_flow_style=False))
-
-
+    gold_standard = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
+    print(gold_standard, input_file, output_file)
+    with open(gold_standard, 'r') as g_s:
+        g_s_config = yaml.load(g_s)
+    with open(input_file, 'r') as i_f:
+        i_f_config = yaml.load(i_f)
+    ''' LEVEL 1 '''
+    for key in g_s_config["ora_asm"][0]:
+        print("- Level 1 list - key " + str(key) + " - Type - " + str(type(g_s_config["ora_asm"][0][key])))
+        key_l1 = g_s_config["ora_asm"][0][key]
+        ''' LEVEL 1 list '''
+        if type(key_l1) is list:
+            ''' LEVEL 2 '''
+            for element in range(len(g_s_config["ora_asm"][0][key])):
+                print("    - Level 2 - key " + str(element) + " - Type - " + str(type(g_s_config["ora_asm"][0][key][element])))
+                key_l2 = g_s_config["ora_asm"][0][key][element]
+                if type(key_l2) is dict:
+                    ''' LEVEL 3 '''
+                    for item in g_s_config["ora_asm"][0][key][element]:
+                        print("        - Level 3 - key " + str(item) + " - Type - " + str(type(g_s_config["ora_asm"][0][key][element][item])))
+                        ''' try will add entries only into existing blocks '''
+                        try:
+                            if item not in i_f_config["ora_asm"][0][key][element]:
+                                i_f_config["ora_asm"][0][key][element][item] = ''
+                        except:
+                            pass
+        ''' LEVEL 1 string '''
+        if type(key_l1) is str:
+            print("    - Level 1 string - key " + str(key) + " - Type - " + str(type(g_s_config["ora_asm"][0][key][element])))
+            if key not in i_f_config["ora_asm"][0]:
+                i_f_config["ora_asm"][0][key] = ''
+    with open(output_file, 'w') as o_f:
+        o_f.write(yaml.dump(i_f_config, default_flow_style=False))
 
 
 if __name__ == '__main__':
     main()
-
