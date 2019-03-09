05b2e8d14428293a935934abc25b841af5af079b
sergiimarchuk <sergii.marchuk@gmail.com>
2019-03-09 14:44:40 +0200

    Update ping_check_scan_hosts_subnet.py

--- ping_check_scan_hosts_subnet.py
+++ ping_check_scan_hosts_subnet.py
@@ -11,3 +11,20 @@ for i in range(1,255,1):
 	#print output
 	if "Unreachable" not in output:
 		print output
+
+		
+#!/usr/bin/python
+import subprocess
+print "test"
+i=0
+while i in range(1,255,1):
+	i = i + 1
+	print i	
+
+	args = ("/bin/ping", "-c", "3", "192.168.1." + str(i))
+	popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+	popen.wait()
+	output = popen.stdout.read()
+	#print output
+	if "Unreachable" not in output:
+		print output
